289197682dbaa40fdea5d5d625c0008ca7e0ccbf
superpuper777 <superfevrik@gmail.com>
2025-04-18 22:02:34 +0300

    feat(strapi): configure CORS for Next.js frontend

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000',
+        'https://thinkit-grze.onrender.com',
+        'https://strapi-thinkit.onrender.com'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
